65d9cc31b2ff472a65d5b17b9b25d4ed6520ff8a
langtonhugh <slangton@nscr.nl>
2024-02-13 16:12:32 +0100

    Create project
    Project, code and folder structure for NIPV comparison.

---
+++ scripts/p2000_brandweer_comparison_v2.R
@@ -0,0 +1,172 @@
+# ==============================================
+# Handling and visuals for Brandweer comparison.
+# ==============================================
+
+# Load packages.
+library(dplyr)
+library(readr)
+library(ggplot2)
+library(forcats)
+library(lubridate)
+
+# Turn off scientific notation.
+options(scipen=9999999)
+
+# Set ggplot2 theme.
+theme_set(theme_bw())
+
+# Load in the p2000 data. SL has sent this to the NIPV.
+p2000_df <- read_csv("data/p2000_demo_data.csv")
+
+# Load in the GMS data. For now, this is fake data created by SL.
+gms_df   <- read_csv("data/fake_gms_demo_data.csv")
+
+# We need a day-date to filter the gms data.
+p2000_df <- p2000_df %>% 
+  mutate(full_date = date(timestamp))
+
+# Filter the GMS data according to days present in the p2000 data. In this
+# demo, the fake GMS data is just a random sample of the p2000 data, so the
+# filter probably does nothing. The real thing will drop much more though
+# (as it should!).
+gms_sub_df <- gms_df %>% 
+  mutate(full_date = date(timestamp )) %>% 
+  filter(full_date %in% unique(p2000_df$full_date))
+
+# Bind them together to make handling easier.
+complete_df <- bind_rows(p2000_df, gms_sub_df)
+
+# Remove the old ones to save memory if needed.
+rm(gms_df, gms_sub_df, p2000_df)
+
+# Create (potentially) useful variables.
+clean_df <- complete_df %>% 
+  mutate(date_time_r  = round_date(timestamp, "hour"),
+         hour_of_day  = hour(date_time_r),
+         week_day     = wday(full_date , abbr = FALSE, label = TRUE),
+         hour_of_week = hour_of_day + (24 * (wday(full_date, week_start = 1)-1)), # Create an hour of the week (0-167 hours).
+         month_naam   = month(full_date, abbr = FALSE, label = TRUE),
+         week_year    = week(full_date),
+         week_day_f   = fct_relevel(week_day,
+                          "maandag"  , "dinsdag" , "woensdag",
+                          "donderdag", "vrijdag" , "zaterdag", "zondag")
+         )
+
+# To speed things up and try things out, use random sample. 
+# Otherwise, comment this out.
+clean_df <- clean_df %>%
+  sample_n(size = 100000) %>%
+  ungroup()
+
+# Nationwide hourly frequencies by hour (mainly to show study period and gaps).
+clean_df %>% 
+  group_by(date_time_r, data_source) %>% 
+  summarise(counts = n()) %>% 
+  ungroup() %>% 
+  ggplot() +
+  geom_line(mapping = aes(x = date_time_r, y = counts, colour = data_source)) +
+  facet_wrap(~data_source, scales = "free", ncol = 1) +
+  theme(legend.position = "none")
+
+# Save.
+ggsave(filename = "visuals/hourly_counts_study_period.png",
+       height = 10, width = 16, unit = "cm")
+
+# Check regional frequencies.
+clean_df %>% 
+  ggplot() +
+  geom_bar(mapping = aes(x = region, fill = data_source), position = "dodge") + 
+  coord_flip() +
+  theme(legend.position = "bottom")
+
+# Save.
+ggsave(filename = "visuals/regional_frequencies.png",
+       height = 16, width = 12, unit = "cm")
+
+# Check priority frequencies by region.
+clean_df %>% 
+  ggplot() +
+  geom_bar(mapping = aes(x = priority, fill = data_source), position = "dodge") +
+  facet_wrap(~region) +
+  theme(legend.position = "bottom")
+
+# Save.
+ggsave(filename = "visuals/priority_frequencies_region.png",
+       height = 26, width = 28, unit = "cm")
+
+# Regional frequency counts.
+regional_hour_freq_df <- clean_df %>%
+  group_by(region, data_source, hour_of_day) %>%
+  tally() %>% 
+  ungroup()
+
+# Plot.
+ggplot(data = regional_hour_freq_df) +
+  geom_line(mapping = aes(x = hour_of_day, y = n, group = data_source, colour = data_source)) +
+  facet_wrap(~region, scales = "free") +
+  labs(colour = NULL) +
+  theme(
+    legend.position = "bottom"
+  )
+
+# Save.
+ggsave(filename = "visuals/regional_hourly_frequencies.png",
+       height = 26, width = 28, unit = "cm")
+
+# Distributions plots of hours of day.
+hourly_counts_df <- clean_df %>% 
+  group_by(hour_of_day, data_source, full_date) %>% 
+  summarise(counts = n()) %>% 
+  ungroup() 
+
+# # I've commented this out because the boxplots below are maybe better alternative.
+# # Plot hours of the day means with raw distribution around it.
+# hourly_counts_df %>%
+#   filter(counts < 50) %>% # Comment out or change (are you using the sample?).
+#   ggplot(mapping = aes(x = hour_of_day, y = counts, group = 1, col = data_source),
+#          shape = 21, fill = "black", colour = NA) +
+#   geom_point(alpha = 0.1) +
+#   stat_summary(fun = mean, geom = "line" , col = "black", group = 1, linewidth = 1) +
+#   facet_wrap(~data_source, scales = "free_y", nrow = 1) +
+#   theme(legend.position = "none")
+# 
+# # Save.
+# ggsave(filename = "visuals/hours_mean_distribution.png",
+#        height = 26, width = 28, unit = "cm")
+
+# Plot distribution.
+hourly_counts_df %>% 
+  filter(counts < 50) %>% # Comment out or change (are you using the sample?).
+  ggplot(mapping = aes(x = hour_of_day, y = counts, group = hour_of_day, fill = data_source)) +
+  geom_boxplot(outlier.shape = NA) + # this removes outlier points.
+  stat_summary(fun = mean, geom = "point", group = 1) +
+  facet_wrap(~data_source, scales = "fixed", nrow = 1) +
+  theme(legend.position = "none")
+
+# Save.
+ggsave(filename = "visuals/hours_mean_boxplots.png",
+       height = 26, width = 28, unit = "cm")
+
+# Create hour of week distributions.
+hour_week_counts_df <- clean_df %>% 
+  group_by(hour_of_week, full_date, data_source) %>% 
+  summarise(counts = n()) %>% 
+  ungroup()
+
+# Plot distribution.
+hour_week_counts_df %>% 
+  filter(counts < 50) %>% # Comment out or change (are you using the sample?).
+  ggplot(mapping = aes(x = hour_of_week, y = counts, group = hour_of_week, colour = data_source)) +
+  geom_point(alpha = 0.1) +
+  stat_summary(fun = "mean", geom = "line", col = "black", group = 1) +
+  facet_wrap(~data_source, nrow = 2, scales = "free_y") +
+  theme(legend.position = "none")
+
+# Save.
+ggsave(filename = "visuals/hours_week_mean_disributions.png",
+       height = 12, width = 18, unit = "cm")
+
+
+# End.
+
+
---
+++ scripts/p2000_brandweer_create_data_v2.R
@@ -0,0 +1,89 @@
+# =======================================
+# Loading in real P2000 data and creating
+# fake 'real' GMS data.
+# =======================================
+
+# Load packages.
+library(readr)
+library(dplyr)
+library(stringr)
+library(forcats)
+library(tidyr)
+library(lubridate)
+
+# Turn off scientific notation.
+options(scipen=9999999)
+
+# Load data. This has been handled and saved in p2000_html_handling_use_this.R script.
+mydata_1 <- readRDS("data/mydata_20211130_20220524.rds")
+mydata_2 <- readRDS("data/new_scrapes_20220524_20230208.rds")
+mydata_3 <- readRDS("data/new_scrapes_20230208_20231009.rds")
+
+# Bind rows.
+mydata <- bind_rows(mydata_1, mydata_2, mydata_3)
+
+# Remove old to save memory if needed.
+rm(mydata_1, mydata_2, mydata_3)
+
+# Initial cleaning.
+source_clean_df <- mydata %>% 
+  rename(code = X1, times = X2, dates = X3, sign = X4, info = X5) %>% 
+  drop_na(times) %>%  
+  distinct(times, dates, info, .keep_all = TRUE) %>% 
+  mutate(service = case_when(str_detect(string = info, pattern = "Ambulance" ) ~ "Ambulance", # imperfect.
+                             str_detect(string = info, pattern = "Politie"   ) ~ "Police",
+                             str_detect(string = info, pattern = "Brandweer" ) ~ "Fire",
+                             str_detect(string = info, pattern = "MKA"       ) ~ "Ambulance"),
+         priority = str_remove_all(string = str_extract_all(
+           info, "^.{3}"), pattern = " ")
+         ) 
+
+# Create a complete date variable to mimic GMS data.
+source_clean_df <- source_clean_df %>% 
+  mutate(timestamp = dmy_hms(paste(dates, times)))
+
+# Clean the data in a way which kind of mimics GMS data for fire only, but we 
+# also keep the info variable so NIPV see the raw info.
+source_gms_struc_df <- source_clean_df %>% 
+  filter(priority %in% c("P1", "P2"),   
+         service  == "Fire") %>% 
+  select(code, timestamp, sign, info, service, priority)
+
+# Load in the CAP code data.
+cap_df <- read_delim("data/capcodelijst_source_in_header.csv", skip = 1, delim = ";") %>% 
+  mutate(code = as.character(code))
+
+# Get a table of what ones we have. First-two-digits ID is not that simple...
+fire_cap_lookup_df <- cap_df %>% 
+  filter(cap_service == "Brandweer") %>% 
+  mutate(region_code = str_extract_all(code, "^.{2}")) %>% 
+  group_by(region_code, region) %>% 
+  tally() %>% 
+  ungroup() %>% 
+  arrange(region_code)
+
+# Join cap codes with the scraped data, create label for the data 'source'
+# and then drop missing regions.
+fire_clean_df <- source_gms_struc_df %>% 
+  left_join(cap_df, relationship = "many-to-many") %>% 
+  mutate(data_source = "p2000")
+
+# How many are missing on the 'region'? 
+sum(is.na(fire_clean_df$region)) / nrow(fire_clean_df) # 25%
+
+# Drop missings on region.
+# !!! note this decision !!! you may decide to keep them !!!
+fire_no_miss_clean_df <- fire_clean_df %>%
+  filter(!is.na(region) )
+
+# Mimic this data for 'fake' real GMS data.
+fake_gms_df <- fire_no_miss_clean_df %>% 
+  slice_sample(prop = 0.75, replace = FALSE) %>% 
+  mutate(info = "No info",
+         data_source = "fake gms") 
+  
+# Save each data separately.
+write_csv(x = fire_no_miss_clean_df, file = "data/p2000_demo_data.csv")
+write_csv(x = fake_gms_df          , file = "data/fake_gms_demo_data.csv")
+
+# End.
